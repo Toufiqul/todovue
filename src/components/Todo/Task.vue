589fa59b77465c37d56477652cb4127b6d32c970
Toufiqul <fahim.prime@gmail.com>
2024-03-13 14:29:59 +0600

    feat: task menu

--- src/components/Todo/Task.vue
+++ src/components/Todo/Task.vue
@@ -16,41 +16,31 @@
           >
         </v-list-item-content>
         <v-list-item-action>
-          <v-btn @click.stop="dialogs.delete = true" icon>
-            <v-icon color="primary lighten-1">mdi-delete</v-icon>
-          </v-btn>
+            <task-menu :task="task"/>
         </v-list-item-action>
       </template>
     </v-list-item>
 
     <v-divider></v-divider>
-    <dialog-delete
-      v-if="dialogs.delete"
-      @close="dialogs.delete = false"
-      :task="task"
-    />
+
   </div>
 </template>
 
 <script>
 export default {
   props: ["task"],
-  data() {
-    return {
-      dialogs: {
-        delete: false,
-      },
-    };
-  },
 //   created() {
 //     console.log(this.task.id);
 //   },
   components: {
-    "dialog-delete": require("@/components/Todo/Dialogs/DialogDelete.vue")
-      .default,
+
+    "task-menu": require("@/components/Todo/TaskMenu.vue").default
+    
   },
 };
+
 </script>
+
 
 <style>
 </style>
