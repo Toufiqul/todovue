18d456b6136a3d004ff05103579cd2f8c7120d0f
Toufiqul <fahim.prime@gmail.com>
2024-03-19 09:42:01 +0600

    feat: search bar

--- src/App.vue
+++ src/App.vue
@@ -36,23 +36,15 @@
       <v-toolbar-title>Vuetify Todo</v-toolbar-title>
 
       <v-spacer></v-spacer>
-
+        <search />
       <v-btn icon>
         <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
       </v-btn>
     </v-app-bar>
 
     <v-main>
       <router-view></router-view>
-      <snackbar/>
+      <snackbar />
     </v-main>
   </v-app>
 </template>
@@ -66,8 +58,9 @@ export default {
       { title: "About", icon: "mdi-help-box", to: "/about" },
     ],
   }),
-  component: {
-    'snackbar': require("@/components/Shared/Snackbar.vue").default
+  components: {
+    search: require("@/components/Tools/Search.vue").default, 
+    snackbar: require("@/components/Shared/Snackbar.vue").default,
   },
 };
 </script>
---
+++ src/components/Tools/Search.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-text-field
+    placeholder="Search"
+    prepend-inner-icon="mdi-magnify"
+    dense
+    clearable
+    filled
+    ></v-text-field>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
